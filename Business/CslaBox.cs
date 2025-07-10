45e36d7e220c46ea885fa9f7240ff8b4ca71ba47
Jaques Oosthuysen <jaques.oosthuysen@wolfsystem.co.uk>
2025-07-10 15:08:33 +0100

    CslaBox - add TryGetPortal

--- Business/CslaBox.cs
+++ Business/CslaBox.cs
@@ -1,7 +1,19 @@
-﻿namespace Business
+﻿using Csla;
+
+namespace Business
 {
     public static class CslaBox
     {
         public static IServiceProvider ServiceProvider { get; set; }
+
+        public static bool TryGetPortal<T>(out IDataPortal<T> dataportal)
+        {
+            dataportal = default;
+            if (ServiceProvider == null) return false;
+            var service = ServiceProvider.GetService(typeof(IDataPortalFactory));
+            if (!(service is IDataPortalFactory factory)) return false;
+            dataportal = factory.GetPortal<T>();
+            return dataportal != null;
+        }
     }
 }
