2783dc14c9c704c0d5649c9d0ec33e0aa28b3667
Jaques Oosthuysen <jaques.oosthuysen@wolfsystem.co.uk>
2025-07-10 15:08:50 +0100

    RandomNumberCommand - add impl

--- Business/RandomNumberCommand.cs
+++ Business/RandomNumberCommand.cs
@@ -5,6 +5,25 @@ namespace Business
     [Serializable]
     public class RandomNumberCommand : CommandBase<RandomNumberCommand>
     {
+        public static readonly PropertyInfo<int> ResultProperty = RegisterProperty<int>(c => c.Result);
+        public int Result
+        {
+            get => ReadProperty(ResultProperty);
+            set => LoadProperty(ResultProperty, value);
+        }
 
+        public static RandomNumberCommand Execute()
+        {
+            if (!CslaBox.TryGetPortal<RandomNumberCommand>(out var portal)) return null;
+            var cmd = portal.Create();
+            return portal.Execute(cmd);
+        }
+
+        [Execute]
+        void DoExecute()
+        {
+            var obj = new Random();
+            Result = obj.Next();
+        }
     }
 }
