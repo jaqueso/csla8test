bbd55deb9f84384265768cbde2e26dc61c6d840d
Jaques Oosthuysen <jaques.oosthuysen@wolfsystem.co.uk>
2025-07-14 12:50:44 +0100

    TryGetPortal - create from app context

--- Business/CslaBox.cs
+++ Business/CslaBox.cs
@@ -1,18 +1,17 @@
-﻿using Csla;
+﻿using System;
+using Csla;
+using Csla.Core;
 
 namespace Business
 {
     public static class CslaBox
     {
-        public static IServiceProvider ServiceProvider { get; set; }
+        public static IServiceProvider? ServiceProvider { get; set; }
 
         public static bool TryGetPortal<T>(out IDataPortal<T> dataportal)
         {
-            dataportal = default;
-            if (ServiceProvider == null) return false;
-            var service = ServiceProvider.GetService(typeof(IDataPortalFactory));
-            if (!(service is IDataPortalFactory factory)) return false;
-            dataportal = factory.GetPortal<T>();
+            var context = CslaBox.ServiceProvider?.GetService(typeof(ApplicationContext)) as ApplicationContext;
+            dataportal = context?.GetRequiredService<IDataPortal<T>>();
             return dataportal != null;
         }
     }
