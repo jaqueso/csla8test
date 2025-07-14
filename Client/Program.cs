d26a39ad2f905410e37ca0739cd89ce82e2c448e
Jaques Oosthuysen <jaques.oosthuysen@wolfsystem.co.uk>
2025-07-14 12:52:10 +0100

    Client - set portal

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,16 +1,17 @@
-﻿using Business;
-using Csla;
+﻿using System.Security.Claims;
+using Business;
 using Csla.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using CslaBox = Business.CslaBox;
 
 var services = new ServiceCollection();
 services.AddTransient<HttpClient>();
 
-services.AddCsla(o => o
-  .Security(o => o.FlowSecurityPrincipalFromClient = false)
-  .DataPortal(o => o.ClientSideDataPortal(o => o
-    .UseHttpProxy(o => o.DataPortalUrl = "/api/DataPortal"))));
+var portal = "https://localhost:7282";
+services.AddCsla(
+    o => o.Security(o => o.FlowSecurityPrincipalFromClient = false)
+        .DataPortal(o => o.ClientSideDataPortal(
+            o => o.UseHttpProxy(o => o.DataPortalUrl = portal + "/api/DataPortal")))
+  );
 
 CslaBox.ServiceProvider = services.BuildServiceProvider();
 
